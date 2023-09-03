f7773f4479f5df338d57f7812d4f9eded9100200
Daniel Di Venere <danieldivenere@gmail.com>
2023-09-03 15:46:02 -0700

    nodes seem to be working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+import { createRef, useEffect } from "react";
 import { StyledApp } from "./App.styled";
+import { chart } from "./utils/graph";
 
 function App() {
+  const ref = createRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (!ref.current) {
+      console.log('hi1')
+      return;
+    }
+
+    const { node, simulation, cleanup } = chart(ref.current);
+
+    return () => {
+      cleanup()
+    };
+  }, []);
+
   return (
     <StyledApp>
       <p>Hi!</p>
+      <div ref={ref} />
     </StyledApp>
   );
 }
