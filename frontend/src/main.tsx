a3386f0731022993e2a9264969bec95e74aec69d
Daniel Di Venere <danieldivenere@gmail.com>
2023-09-29 21:10:08 -0700

    still use inter JS

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "@fontsource/inter";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
