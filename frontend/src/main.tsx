280af920d2a2ef2ae1cae52ace11c6c1697dcc61
Daniel Di Venere <danieldivenere@gmail.com>
2023-09-29 21:17:53 -0700

    css order

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import "@fontsource/inter";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/600.css";
+import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 
