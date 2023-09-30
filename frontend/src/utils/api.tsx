ba53a50430f9d8dfa01ee0ab431522fb2316b06e
Daniel Di Venere <danieldivenere@gmail.com>
2023-09-29 20:17:42 -0700

    fix broken api call

--- frontend/src/utils/api.tsx
+++ frontend/src/utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
