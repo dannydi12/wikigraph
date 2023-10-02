d337a16b35f9ee3e5ebdf1236d170530aa6a71b9
Daniel Di Venere <danieldivenere@gmail.com>
2023-10-01 17:43:12 -0700

    search updates

--- frontend/src/Search/Search.styled.tsx
+++ frontend/src/Search/Search.styled.tsx
@@ -21,6 +21,31 @@ export const StyledSearch = styled.div<{ $areResults: boolean }>`
     border: 1px solid #6e6e6e;
     transition: border-color linear 500ms;
     transition: background-color linear 300ms;
+    padding-left: 15px;
+
+    .search-icon {
+      width: 25px;
+      color: #979797;
+    }
+
+    input {
+      padding-left: 12px;
+      background-color: #404040;
+      border: none;
+      border-radius: 10px;
+      transition: background-color linear 300ms;
+      flex-grow: 2;
+      color: white;
+      font-size: 20px;
+
+      &:focus {
+        outline: none;
+      }
+
+      &:hover {
+        background-color: #353535;
+      }
+    }
 
     &:hover {
       background-color: #353535;
@@ -32,19 +57,25 @@ export const StyledSearch = styled.div<{ $areResults: boolean }>`
       border-bottom-left-radius: 0;
     `}
 
-    padding-left: 20px;
-    font-size: 20px;
-    flex-grow: 2;
-
-    color: white;
-
-    &:focus {
+    &:focus-within {
       outline: none;
       border: 1px solid #3183ba;
     }
   }
 
+  .results {
+    max-height: 500px;
+    overflow: scroll;
+    border-bottom-right-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+
   @media (min-width: 800px) {
     max-width: 500px;
+
+    .results {
+      overflow: auto;
+      max-height: none;
+    }
   }
 `;
--- frontend/src/Search/Search.tsx
+++ frontend/src/Search/Search.tsx
@@ -8,6 +8,7 @@ import { suggestedSearches } from "../utils/constants";
 import { randomSearchRecommendation } from "../utils/randomeSearchRecommendation";
 import { useDebounce } from "usehooks-ts";
 import { setCurrentSearch, useAppDispatch, useAppSelector } from "../redux";
+import { ReactComponent as SearchIcon } from "./SearchIcon.svg";
 
 const Search: FC = () => {
   const dispatch = useAppDispatch();
@@ -65,12 +66,14 @@ const Search: FC = () => {
 
   return (
     <StyledSearch $areResults={!!results.length}>
-      <input
-        className="input-box"
-        value={searchInputValue}
-        onChange={(e) => setSearchInputValue(e.target.value)}
-        placeholder={placeholderText}
-      />
+      <div className="input-box">
+        <SearchIcon className="search-icon" />
+        <input
+          value={searchInputValue}
+          onChange={(e) => setSearchInputValue(e.target.value)}
+          placeholder={placeholderText}
+        />
+      </div>
 
       <div className="results">
         {results.map((result) => (
@@ -78,8 +81,8 @@ const Search: FC = () => {
             key={result.title}
             result={result}
             onClick={() => {
-              window.gtag('event', 'search', {
-                'title': result.title
+              window.gtag("event", "search", {
+                title: result.title,
               });
 
               setHasSearchAtLeastOnce(true);
